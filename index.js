30682dde537ea57ea741ec53701ea68a7f8cc14b
shady-mungai <shadrackmungai10@gmail.com>
2022-11-29 11:38:41 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function (){
+    return 2000/100;
+}
+const square = number => number * number
+
+console.log(square(4))
+ 
+const add = (parameterOne, parameterTwo) => parameterOne + parameterTwo
